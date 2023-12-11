826c643a10466c69d0de8ec4cf286e2b907b8b58
alexsharich <alexsharich@gmail.com>
2023-12-11 20:44:20 +0100

    feat: add MyCard, CardList components

---
+++ src/components/cardList/CardList.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MyCard from '../card/MyCard.vue'
+const onClickAdd = ()=>{}
+</script>
+
+
+<template>
+    <div class="grid grid-cols-4 gap-5">
+        <MyCard imageUrl="/sneakers/sneakers-1.jpg" :price="120" title="NIKE" :onClickAdd="onClickAdd" /> 
+</div>
+</template>
---
+++ src/components/header/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="flex justify-between  px-10 py-8 border-b border-slate-100">
+            <div class="flex items-center gap-4">
+                <img class="w-10" src="/logo.png" alt="logo">
+                <div>
+                    <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+                    <p class="text-slate-500">Super Shop</p>
+                </div>
+            </div>
+            <ul class="flex items-center gap-10">
+                <li class="flex items-center gap-3 text-gray-400 cursor-pointer hover:text-black" >
+                    <img src="/cart.svg" alt="Cart">
+                    <b>1250$</b>
+                </li>
+                <li class="flex items-center gap-3 text-gray-400 cursor-pointer hover:text-black" >
+                    <img src="/heart.svg" alt="Cart">
+                    <span>Favorites</span>
+                </li>
+                <li class="flex items-center gap-3 text-gray-400 cursor-pointer hover:text-black" >
+                    <img src="/profile.svg" alt="Cart">
+                    <span>Profile</span>
+                </li>
+            </ul>
+        </header>
+</template>
+<style>
+</style>
