71d1194aea3a51eddc04e2d84200dbadd5cff74e
alexsharich <alexsharich@gmail.com>
2023-12-13 16:43:18 +0100

    feat: add filters

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,72 @@
 <script setup>
-import MyHeader from './components/header/MyHeader.vue';
+import MyHeader from './components/header/MyHeader.vue'
 import CardList from './components/cardList/CardList.vue'
+
+import { onMounted, ref, watch, reactive } from 'vue'
+import axios from 'axios'
 /* import MyDrawer from './components/drawer/MyDrawer.vue' */
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+const items = ref([])
+const sortBy = ref('')
+const sortQuery = ref('')
+
+const onChangeSelect = (e) => {
+  filters.sortBy = e.target.value
+}
+const onChangeQueryInput = (e) => {
+  filters.searchQuery = e.target.value
+}
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+    const { data } = await axios.get(`https://57aeef1f4f92097e.mokky.dev/items`, { params })
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+}
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
-    <!-- <MyDrawer/> -->
-    <div class="rounded-xl bg-white w-4/5 m-auto shadow-xl mt-10">
-        <MyHeader/>
-        <div class="p-10">
-            <div class="flex justify-between items-center">
-                <h2 class="text-3xl font-bold mb-8">ALL SNEAKERS</h2>
+  <!-- <MyDrawer/> -->
+  <div class="rounded-xl bg-white w-4/5 m-auto shadow-xl mt-10">
+    <MyHeader />
+    <div class="p-10">
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">ALL SNEAKERS</h2>
 
-                <div class="flex gap-4">
-                    <select class="py-2 px-3 border rounded outline-none">
-                    <option>Name</option>
-                    <option>Price</option>
-                    <option>Price +</option>
-                    <option>Price -</option>
-                </select>
+        <div class="flex gap-4">
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded outline-none">
+            <option value="name">Name</option>
+            <option value="price">Price +</option>
+            <option value="-price">Price -</option>
+          </select>
 
-                <div class="relative">
-                    <img  class="absolute left-3 top-3" src="/search.svg" alt="Search">
-                    <input class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400" type="text" placeholder="search...">
-                </div>
-                </div>
-            </div>
-                <CardList/>
-        </div>        
+          <div class="relative">
+            <img class="absolute left-3 top-3" src="/search.svg" alt="Search" />
+            <input
+              @input="onChangeQueryInput"
+              class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="search..."
+            />
+          </div>
+        </div>
+      </div>
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
+  </div>
 </template>
-
-
--- src/components/cardList/CardList.vue
+++ src/components/cardList/CardList.vue
@@ -1,11 +1,20 @@
 <script setup>
 import MyCard from '../card/MyCard.vue'
-const onClickAdd = ()=>{}
+const onClickAdd = () => {}
+defineProps({
+  items: Array
+})
 </script>
 
-
 <template>
-    <div class="grid grid-cols-4 gap-5">
-        <MyCard imageUrl="/sneakers/sneakers-1.jpg" :price="120" title="NIKE" :onClickAdd="onClickAdd" /> 
-    </div>
+  <div class="grid grid-cols-4 gap-5">
+    <MyCard
+      v-for="item in items"
+      :key="item.id"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
+      :title="item.title"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
 </template>
